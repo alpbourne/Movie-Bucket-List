08328ea73701d550c747eaf66b4c4f521ecd0867
alpbourne <alpbourne@gmail.com>
2018-02-26 11:29:37 -0500

    Movie prototype

---
+++ app/assets/javascripts/bucket_list_movies.js
@@ -0,0 +1,9 @@
+// function BucketListMovie(bucket_list_id, movie_id, seen) {
+//   this.bucket_list_id = bucket_list_id
+//   this.movie_id = movie_id
+//   this.seen = seen
+// }
+//
+// BucketListMovie.prototype.renderBucketListMovies = function(){
+//   return ''
+// }
---
+++ app/assets/javascripts/bucket_lists.js
@@ -0,0 +1,9 @@
+function Movie(name, genre, rating) {
+  this.name = name
+  this.genre = genre
+  this.rating = rating
+}
+
+Movie.prototype.renderMovies = function(){
+  return ''
+}
