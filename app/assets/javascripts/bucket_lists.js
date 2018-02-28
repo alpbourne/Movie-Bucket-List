e055806346dd2a6712fd66d7ac9bbd200aab0684
alpbourne <alpbourne@gmail.com>
2018-02-28 17:29:09 -0500

    adding jquery/ajax to bucket_list.js

--- app/assets/javascripts/bucket_lists.js
+++ app/assets/javascripts/bucket_lists.js
@@ -1,9 +1,21 @@
-function Movie(name, genre, rating) {
-  this.name = name
-  this.genre = genre
-  this.rating = rating
-}
+$(document).ready(function(){
+  $("a.load_comments").on("click", function(e){
+    alert("yayy!")
+    e.preventDefault()
+  })
+})
 
-Movie.prototype.renderMovies = function(){
-  return ''
-}
+
+
+
+
+
+// function Movie(name, genre, rating) {
+//   this.name = name
+//   this.genre = genre
+//   this.rating = rating
+// }
+//
+// Movie.prototype.renderMovies = function(){
+//   return ''
+// }
