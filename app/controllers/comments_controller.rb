2325d99513f1239e5b2cbe9392f8f002e4ab6693
alpbourne <alpbourne@gmail.com>
2018-03-07 13:45:54 -0500

    bucketlist comment serializer is fixed and comments are loading on movie show page still

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
   def create
     @comment = @bucket_list.comments.build(comment_params)
     if @comment.save
+      binding.pry
       render json: @comment, status: 201
     else
       render "bucket_lists/show"
